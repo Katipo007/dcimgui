4ad1abcd06d68aba26b81361083b57dc6ae30896
Ben Edwards <ben@benedwards.dev>
2026-05-26 20:55:33 +1200

    Remove the need to compile imgui to a library, and allow the consumer of the package to supply a system include path for Emscripten instead of them having to inject it into artifacts generated by this package

--- build.zig
+++ build.zig
@@ -19,7 +19,6 @@ const imgui_sources = [_][]const u8{
 pub const Config = struct {
     module_name: []const u8, // cimgui or cimgui_docking
     include_dir: []const u8, // src or src-docking
-    clib_name: []const u8, // cimgui_clib or cimgui_docking_clib
 };
 
 // helper function to return a matching set of Zig module name,
@@ -29,13 +28,11 @@ pub fn getConfig(docking: bool) Config {
         return .{
             .module_name = "cimgui_docking",
             .include_dir = "src-docking",
-            .clib_name = "cimgui_docking_clib",
         };
     } else {
         return .{
             .module_name = "cimgui",
             .include_dir = "src",
-            .clib_name = "cimgui_clib",
         };
     }
 }
@@ -43,7 +40,7 @@ pub fn getConfig(docking: bool) Config {
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const opt_dynamic_linkage = b.option(bool, "dynamic_linkage", "Builds cimgui_clib artifact with dynamic linkage.") orelse false;
+    const opt_clib_include_path = b.option(std.Build.LazyPath, "clib_include_path", "");
 
     // the regular imgui module
     try buildModule(b, .{
@@ -52,7 +49,7 @@ pub fn build(b: *std.Build) !void {
         .sources = &imgui_sources,
         .target = target,
         .optimize = optimize,
-        .linkage = if (opt_dynamic_linkage) .dynamic else .static,
+        .system_include_paths = if (opt_clib_include_path) |path| &.{path} else &.{},
     });
 
     // ...and the imgui_docking module
@@ -62,7 +59,7 @@ pub fn build(b: *std.Build) !void {
         .sources = &imgui_sources,
         .target = target,
         .optimize = optimize,
-        .linkage = if (opt_dynamic_linkage) .dynamic else .static,
+        .system_include_paths = if (opt_clib_include_path) |path| &.{path} else &.{},
     });
 }
 
@@ -72,22 +69,26 @@ const BuildModuleOptions = struct {
     sources: []const []const u8,
     target: Build.ResolvedTarget,
     optimize: std.builtin.OptimizeMode,
-    linkage: std.builtin.LinkMode,
+    system_include_paths: []const std.Build.LazyPath = &.{},
 };
 
-fn translateC(b: *std.Build, options: std.Build.Step.TranslateC.Options) ?std.Build.LazyPath {
-    const dep_translate_c = b.lazyDependency("translate_c", .{}) orelse return null;
-    const mod_translate_c = b.lazyImport(@This(), "translate_c") orelse return null;
-    const tc: mod_translate_c.Translator = .init(dep_translate_c, .{
-        .c_source_file = options.root_source_file,
-        .target = options.target,
-        .optimize = options.optimize,
-        .link_libc = options.link_libc,
-    });
-    return tc.output_file;
-}
-
 fn buildModule(b: *std.Build, opts: BuildModuleOptions) !void {
+    // translate-c the cimgui.h file for the generated bindings
+    // NOTE: DO NOT USE cimgui_all.h HERE SINCE IT PULLS IN cimgui_internal.h
+    // which doesn't work with Zig's translateC step because this
+    // pulls in C bitfields which are not supported by translateC
+    const c_bindings = b.addTranslateC(.{
+        .root_source_file = b.path(b.fmt("{s}/cimgui.h", .{opts.subdir})),
+        .target = opts.target,
+        .optimize = opts.optimize,
+        .link_libc = true,
+    });
+    for (opts.system_include_paths) |system_include|
+        c_bindings.addSystemIncludePath(system_include);
+
+    const mod = c_bindings.addModule(opts.modname);
+
+    // Compile the imgui sources into the module
     var cflags_buf: [16][]const u8 = undefined;
     var cflags = std.ArrayListUnmanaged([]const u8).initBuffer(&cflags_buf);
     if (opts.target.result.cpu.arch.isWasm()) {
@@ -95,48 +96,12 @@ fn buildModule(b: *std.Build, opts: BuildModuleOptions) !void {
         // but it requires linking with an ubsan runtime)
         try cflags.appendBounded("-fno-sanitize=undefined");
     }
-
-    // build imgui into a C library
-    const mod_clib = b.addModule(b.fmt("mod_{s}_clib", .{opts.modname}), .{
-        .target = opts.target,
-        .optimize = opts.optimize,
-        .link_libc = true,
-        .link_libcpp = true,
-    });
+    for (opts.system_include_paths) |system_include|
+        mod.addSystemIncludePath(system_include);
     for (imgui_sources) |src| {
-        mod_clib.addCSourceFile(.{
+        mod.addCSourceFile(.{
             .file = b.path(b.fmt("{s}/{s}", .{ opts.subdir, src })),
             .flags = cflags.items,
         });
     }
-    const clib = b.addLibrary(.{
-        .name = b.fmt("{s}_clib", .{opts.modname}),
-        .root_module = mod_clib,
-        .linkage = opts.linkage,
-    });
-    // make the C library available as artifact, this allows to inject
-    // the Emscripten sysroot include path in the upstream project
-    b.installArtifact(clib);
-
-    // translate-c the cimgui.h file
-    // NOTE: running this step with the host target is intended to avoid
-    // any Emscripten header search path shenanigans
-    // NOTE: DO NOT USE cimgui_all.h HERE SINCE IT PULLS IN cimgui_internal.h
-    // which doesn't work with Zig's translateC step because this
-    // pulls in C bitfields which are not supported by trandlateC
-    const translate_c_file = translateC(b, .{
-        .root_source_file = b.path(b.fmt("{s}/cimgui.h", .{opts.subdir})),
-        .target = b.graph.host,
-        .optimize = opts.optimize,
-    });
-
-    // ...and the Zig module for the generated bindings
-    const mod = b.addModule(opts.modname, .{
-        .root_source_file = translate_c_file,
-        .target = opts.target,
-        .optimize = opts.optimize,
-        .link_libc = true,
-        .link_libcpp = true,
-    });
-    mod.linkLibrary(clib);
 }
